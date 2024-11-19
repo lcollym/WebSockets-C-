6b60882cc3fef3d7e33a7f6ef36cd671109d1b30
/Luis R Collymoore <lcollymoore@gmail.com>
2024-11-19 14:59:28 -0400

    create server socket

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+class Server {
+    public static void Main()
+    {
+        const string IP = "10.0.0.5";
+        TcpListener server = new TcpListener(IPAddress.Parse(IP), 80);
+
+        server.Start();
+        Console.WriteLine("El server se ha iniciado en" + IP + " :80.{0}Esperando una conexión...", Environment.NewLine);
+
+        TcpClient client = server.AcceptTcpClient();
+
+        Console.WriteLine("Un cliente conectado.");
+    }
+}
+
